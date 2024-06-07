03b0d97f69ce023cfce70724f03f91cc8811331b
myscience <muratore.paolo95@gmail.com>
2024-06-07 19:23:29 +0200

    🐛 Important bug fixes

--- genie/tokenizer.py
+++ genie/tokenizer.py
@@ -14,13 +14,156 @@ from lightning import LightningModule
 
 from genie.module.loss import GANLoss
 from genie.module.loss import PerceptualLoss
+from genie.module.norm import AdaptiveGroupNorm
 from genie.module.quantization import LookupFreeQuantization
-from genie.module.video import CausalConv3d, ResidualBlock
-from genie.module.video import SpaceUpsample
+from genie.module.video import CausalConv3d
+from genie.module.video import DepthToSpaceTimeUpsample
+from genie.module.video import DepthToSpaceUpsample
+from genie.module.video import DepthToTimeUpsample
+from genie.module.video import ResidualBlock
+from genie.module.video import SpaceTimeDownsample
 from genie.utils import default, exists
 
 OptimizerCallable = Callable[[Iterable], Optimizer]
-        
+
+MAGVIT2_ENC_DESC = (
+    ('causal', {
+        'in_channels': 3,
+        'out_channels': 128,
+        'kernel_size': 3,
+    }),
+    ('residual', {
+        'n_rep': 4,
+        'in_channels': 128,
+    }),
+    ('spacetime_downsample', {
+        'in_channels': 128,
+        'out_channels': 128,
+        'kernel_size': 3,
+        'time_factor': 1,
+        'space_factor': 2,
+    }),
+    ('residual', {
+        'in_channels': 128,
+        'out_channels': 256,
+    }),
+    ('residual', {
+        'n_rep': 3,
+        'in_channels': 256,
+    }),
+    ('spacetime_downsample', {
+        'in_channels': 256,
+        'out_channels': 256,
+        'kernel_size': 3,
+        'time_factor': 2,
+        'space_factor': 2,
+    }),
+    ('residual', {
+        'n_rep': 4,
+        'in_channels': 256,
+    }),
+    ('spacetime_downsample', {
+        'in_channels': 256,
+        'out_channels': 256,
+        'kernel_size': 3,
+        'time_factor': 2,
+        'space_factor': 2,
+    }),
+    ('residual', {
+        'in_channels': 256,
+        'out_channels': 512,
+    }),
+    ('residual', {
+        'n_rep': 7,
+        'in_channels': 512,
+    }),
+    ('proj_out', {
+        'in_channels': 512,
+        'out_channels': 128,
+        'num_groups': 8,
+        'kernel_size': 3,
+    })
+)
+
+MAGVIT2_DEC_DESC = (
+    ('causal', {
+        'in_channels': 18,
+        'out_channels': 512,
+        'kernel_size': 3,
+    }),
+    ('residual', {
+        'n_rep': 4,
+        'in_channels': 512,
+    }),
+    ('adaptive_group_norm', {
+        'num_groups': 8,
+        'num_channels': 512,
+        'has_ext' : True,
+    }),
+    ('residual', {
+        'n_rep': 4,
+        'in_channels': 512,
+    }),
+    ('spacetime_upsample', {
+        'in_channels': 512,
+        'kernel_size': 3,
+        'time_factor': 2,
+        'space_factor': 2,
+    }),
+    ('adaptive_group_norm', {
+        'num_groups': 8,
+        'num_channels': 512,
+        'has_ext' : True,
+    }),
+    ('residual', {
+        'in_channels': 512,
+        'out_channels': 256,
+    }),
+    ('residual', {
+        'n_rep': 3,
+        'in_channels': 256,
+    }),
+    ('spacetime_upsample', {
+        'in_channels': 256,
+        'kernel_size': 3,
+        'time_factor': 2,
+        'space_factor': 2,
+    }),
+    ('adaptive_group_norm', {
+        'num_groups': 8,
+        'num_channels': 256,
+        'has_ext' : True,
+    }),
+    ('residual', {
+        'n_rep' : 4,
+        'in_channels': 256,
+    }),
+    ('spacetime_upsample', {
+        'in_channels': 256,
+        'kernel_size': 3,
+        'time_factor': 1,
+        'space_factor': 2,
+    }),
+    ('adaptive_group_norm', {
+        'num_groups': 8,
+        'num_channels': 256,
+    }),
+    ('residual', {
+        'in_channels': 256,
+        'out_channels': 128,
+    }),
+    ('residual', {
+        'n_rep' : 3,
+        'in_channels': 128,
+    }),
+    ('proj_out', {
+        'in_channels': 128,
+        'out_channels': 3,
+        'num_groups': 8,
+        'kernel_size': 3,
+    })
+)
+
 def get_module(name : str) -> nn.Module:
     match name:
         case 'residual':
@@ -28,7 +171,21 @@ def get_module(name : str) -> nn.Module:
         case 'causal':
             return CausalConv3d
         case 'space_upsample':
-            return SpaceUpsample
+            return DepthToSpaceUpsample
+        case 'time_upsample':
+            return DepthToTimeUpsample
+        case 'spacetime_upsample':
+            return DepthToSpaceTimeUpsample
+        case 'spacetime_downsample':
+            return SpaceTimeDownsample
+        case 'adaptive_group_norm':
+            return AdaptiveGroupNorm
+        case 'proj_out':
+            return lambda *args, **kwargs: nn.Sequential(
+                nn.GroupNorm(kwargs.pop('num_groups', 1), kwargs.get('in_channels')),
+                nn.SiLU(),
+                CausalConv3d(**kwargs)
+            )
         case _:
             raise ValueError(f'Unknown module name: {name}')
 
@@ -46,11 +203,7 @@ class VideoTokenizer(LightningModule):
         self,
         enc_desc : Tuple[str | Tuple[str, Dict], ...],
         dec_desc : Tuple[str | Tuple[str, Dict], ...],
-        inp_dim : int = 128,
-        out_dim : int | None = None,
-        inp_channels: int = 3,
-        kernel_size : int | Tuple[int, int, int] = 3,
-        pad_mode : str = 'constant',
+        disc_kwargs : Dict[str, Any] = {},
         # Lookup-Free Quantization parameters
         d_codebook : int = 18,
         n_codebook : int = 1,
@@ -63,7 +216,7 @@ class VideoTokenizer(LightningModule):
         # Misc parameters
         optimizer : OptimizerCallable = AdamW,
         perceptual_model : str = 'vgg16',
-        perc_feat_layers : str | Iterable[str] = ('relu1_2', 'relu2_2', 'relu3_3', 'relu4_3'),
+        perc_feat_layers : str | Iterable[str] = ('features.6', 'features.13', 'features.18', 'features.25'),
         gan_loss_weight : float = 1.,
         perc_loss_weight : float = 1.,
         quant_loss_weight : float = 1.,
@@ -73,30 +226,20 @@ class VideoTokenizer(LightningModule):
         
         self.optimizer = optimizer
         
-        inp_conv = CausalConv3d(
-            inp_channels,
-            inp_dim,
-            kernel_size=kernel_size,
-            pad_mode=pad_mode,
-        )
-        
-        out_conv = CausalConv3d(
-            inp_dim,
-            inp_channels,
-            kernel_size=kernel_size,
-            pad_mode=pad_mode,
-        )
-        
         # Scan the blueprint to build the tokenizer
-        self.enc_layers = nn.ModuleList([inp_conv])
-        self.dec_layers = nn.ModuleList([out_conv])
+        self.enc_layers = nn.ModuleList([])
+        self.dec_layers = nn.ModuleList([])
+        self.enc_ext = list()
+        self.dec_ext = list()
         
         for enc_l, dec_l in zip_longest(enc_desc, dec_desc):
             if isinstance(enc_l, str): enc_l = (enc_l, {})
             if isinstance(dec_l, str): dec_l = (dec_l, {})
             
-            name, kwargs = default(enc_l, (None, None))
-            self.enc_ext = kwargs.pop('has_ext', False)
+            name, kwargs = default(enc_l, (None, {}))
+            self.enc_ext.extend(
+                [kwargs.pop('has_ext', False)] * kwargs.get('n_rep', 1)
+            )
             self.enc_layers.extend(
                 [
                     get_module(name)(**kwargs)
@@ -104,8 +247,10 @@ class VideoTokenizer(LightningModule):
                     if exists(name) and exists(kwargs)
                 ]
             )
-            name, kwargs = default(dec_l, (None, None))
-            self.dec_ext = kwargs.pop('has_ext', False)
+            name, kwargs = default(dec_l, (None, {}))
+            self.dec_ext.extend(
+                [kwargs.pop('has_ext', False)] * kwargs.get('n_rep', 1)
+            )
             self.dec_layers.extend(
                 [
                     get_module(name)(**kwargs)
@@ -116,11 +261,10 @@ class VideoTokenizer(LightningModule):
         
         # Check consistency between last encoder dimension, first
         # decoder dimension and the codebook dimension
-        last_enc_dim = self.enc_layers[-1].out_channels
+        last_enc_dim = list(self.enc_layers.modules())[-1].out_channels
         first_dec_dim = self.dec_layers[0].in_channels
         assert last_enc_dim == first_dec_dim, 'Inconsistent encoder/decoder dimensions'
         assert last_enc_dim == d_codebook   , 'Codebook dimension mismatch with encoder/decoder'
-        
         
         # Build the quantization module
         self.quant = LookupFreeQuantization(
@@ -143,6 +287,7 @@ class VideoTokenizer(LightningModule):
         # If the GAN loss is enabled, load the Discriminator model
         self.gan_crit = GANLoss(
                 num_frames=num_frames_per_batch,
+                **disc_kwargs,
             ) if gan_loss_weight > 0 else nn.Identity()
         
         
@@ -158,7 +303,7 @@ class VideoTokenizer(LightningModule):
     ) -> Tensor:
         enc_video = video
         
-        for layer, has_ext in self.enc_layers:
+        for layer, has_ext in zip(self.enc_layers, self.enc_ext):
             enc_video = layer(enc_video, cond) if has_ext else layer(enc_video)
             
         return enc_video
@@ -181,7 +326,7 @@ class VideoTokenizer(LightningModule):
         self,
         video : Tensor,
         beta : float = 100.,
-        transpose : bool = False,
+        transpose : bool = True,
     ) -> Tuple[Tensor, Tensor]:
         self.eval()
         
@@ -198,7 +343,7 @@ class VideoTokenizer(LightningModule):
         self,
         video : Tensor,
         beta : float = 100.,
-        transpose : bool = False,
+        transpose : bool = True,
     ) -> Tuple[Tensor, Tuple[Tensor, ...]]:
         enc_video = self.encode(video)
         (quant_video, idxs), quant_loss = self.quant(enc_video, beta=beta, transpose=transpose)
@@ -209,7 +354,8 @@ class VideoTokenizer(LightningModule):
         rec_loss = mse_loss(rec_video, video)
         
         # GAN loss (if available)
-        gan_loss = self.gan_crit(rec_video, video)
+        gen_loss = self.gan_crit(rec_video, video, train_gen=True)
+        dis_loss = self.gan_crit(rec_video, video, train_gen=False)
         
         # Perceptual loss (if available)
         perc_loss = self.perc_crit(rec_video, video)
@@ -217,13 +363,15 @@ class VideoTokenizer(LightningModule):
         # Compute the total loss by combining the individual
         # losses, weighted by the corresponding loss weights
         loss = rec_loss\
-            + gan_loss   * self.gan_loss_weight\
+            + gen_loss   * self.gan_loss_weight\
+            + dis_loss   * self.gan_loss_weight\
             + perc_loss  * self.perc_loss_weight\
             + quant_loss * self.quant_loss_weight\
         
         return loss, (
             rec_loss,
-            gan_loss if self.gan_loss_weight > 0 else None,
+            gen_loss if self.gan_loss_weight > 0 else None,
+            dis_loss if self.gan_loss_weight > 0 else None,
             perc_loss if self.perc_loss_weight > 0 else None,
             quant_loss
         )
@@ -239,9 +387,10 @@ class VideoTokenizer(LightningModule):
             {
                 'train_loss': loss,
                 'train_rec_loss'  : aux_losses[0],
-                'train_gan_loss'  : aux_losses[1],
-                'train_perc_loss' : aux_losses[2],
-                'train_quant_loss': aux_losses[3],
+                'train_gen_loss'  : aux_losses[1],
+                'train_dis_loss'  : aux_losses[2],
+                'train_perc_loss' : aux_losses[3],
+                'train_quant_loss': aux_losses[4],
             },
             logger=True,
             on_step=True,
@@ -259,9 +408,10 @@ class VideoTokenizer(LightningModule):
             {
                 'val_loss': loss,
                 'val_rec_loss'  : aux_losses[0],
-                'val_gan_loss'  : aux_losses[1],
-                'val_perc_loss' : aux_losses[2],
-                'val_quant_loss': aux_losses[3],
+                'val_gen_loss'  : aux_losses[1],
+                'val_dis_loss'  : aux_losses[2],
+                'val_perc_loss' : aux_losses[3],
+                'val_quant_loss': aux_losses[4],
             },
             on_step=True,
             logger=True,
