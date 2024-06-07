69c348ef336f224dbcf5e4f06d0ceb629ab037a3
myscience <muratore.paolo95@gmail.com>
2024-06-07 19:21:29 +0200

    🐛 Fixed implementation of AdaptiveGroupNorm

--- genie/module/norm.py
+++ genie/module/norm.py
@@ -1,29 +1,49 @@
+import torch
 import torch.nn as nn
 from torch import Tensor
 from torch.nn.functional import group_norm
 
-from einops import pack, unpack
+from einops import pack, rearrange, unpack
 
-class AdaptiveGroupNorm(nn.GroupNorm):
+class AdaptiveGroupNorm(nn.Module):
     def __init__(
         self,
         dim_cond : int,
         num_groups: int,
         num_channels: int,
         cond_bias : bool = True,
-        **kwargs
+        affine : bool = True,
+        eps : float = 1e-5,
+        device : str | None = None,
+        dtype : str | None = None,
     ) -> None:
-        super().__init__(
-            num_groups=num_groups,
-            num_channels=num_channels,
-            **kwargs
-        )
+        super().__init__()
         
-        self.std = nn.Linear(dim_cond, self.num_groups)
-        self.avg = nn.Linear(dim_cond, self.num_groups) if cond_bias else None
+        if num_channels % num_groups != 0:
+            raise ValueError('num_channels must be divisible by num_groups')
 
-    def reset_parameters(self) -> None:
-        super().reset_parameters()
+        self.num_groups = num_groups
+        self.num_channels = num_channels
+        self.eps = eps
+        self.affine = affine
+        
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        if self.affine:
+            self.weight = nn.Parameter(torch.empty(num_channels, **factory_kwargs))
+            self.bias   = nn.Parameter(torch.empty(num_channels, **factory_kwargs))
+        else:
+            self.register_parameter('weight', None)
+            self.register_parameter('bias', None)
+            
+        self.std = nn.Linear(dim_cond, self.num_channels)
+        self.avg = nn.Linear(dim_cond, self.num_channels) if cond_bias else None
+        
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:      
+        if self.affine:
+            nn.init.ones_(self.weight)
+            nn.init.zeros_(self.bias)
         
         nn.init.ones_ (self.std.bias)
         nn.init.zeros_(self.std.weight)
@@ -38,9 +58,12 @@ class AdaptiveGroupNorm(nn.GroupNorm):
         norm = group_norm(inp, self.num_groups, self.weight, self.bias, self.eps)
         norm, ps = pack([norm], 'b g *')
         
+        # Condition is expected to have shape b d ...
+        cond = rearrange(cond, 'b d ... -> b d (...)').mean(-1)
+        
         # Rescale the normalized input to match the conditional statistics
         std = self.std(cond).unsqueeze(-1)
         avg = self.avg(cond).unsqueeze(-1) if self.avg is not None else 0
         
         out =  norm * std + avg
-        return unpack(out, ps)[0]
+        return unpack(out, ps, 'b g *')[0]
